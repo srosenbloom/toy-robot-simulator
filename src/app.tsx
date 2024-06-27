4891ab9723b155e883095cb8986bcc6f434b0012
Steve Rosenbloom <stever884@gmail.com>
2024-06-26 19:30:29 -0700

    arrows

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,27 @@ import React from 'react';
 import Grid from './grid';
 import Controls from './controls';
 import Console from './console';
+import { useStore } from './store';
 
 import styles from './app.module.css';
 
 const App: React.FC = () => {
+    const { issueCommand } = useStore();
+
+    const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+        if (event.code === "ArrowUp") {
+            issueCommand({ cmd: 'MOVE' });
+        }
+        else if (event.code === "ArrowLeft") {
+            issueCommand({ cmd: 'LEFT' });
+        }
+        else if (event.code === "ArrowRight") {
+            issueCommand({ cmd: 'RIGHT' });
+        }
+    };
+
   return (
-    <div className={styles.app}>
+    <div className={styles.app} onKeyDown={onKeyDown}>
       <Grid />
       <Controls />
       <Console />
